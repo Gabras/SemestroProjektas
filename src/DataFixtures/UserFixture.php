578d006b4ebbd361da575a81fb85c437d7ccdb70
Gabras <gabrieliusjurg@gmail.com>
2020-04-24 23:52:54 +0300

    Done with fixtures

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -23,7 +23,7 @@ class UserFixture extends Fixture
     {
         $faker = Factory::create();
 
-        for ($i = 0; $i < 10; $i++)
+        for ($i = 0; $i < 50; $i++)
         {
             $user = new User();
             $user->setFirstName($faker->firstName);
@@ -31,7 +31,15 @@ class UserFixture extends Fixture
             $user->setEmail($faker->email);
             $user->setPassword($this->passwordEncoder->encodePassword($user, '123456'));
             $user->setUsername($faker->userName);
-            $user->setRole($faker->randomElement($array = array('ROLE_EMPLOYER', 'ROLE_EMPLOYEE')));
+            $ran = RANDOM_INT(1, 10);
+            if($ran > 8)
+            {
+                $user->setRole('ROLE_EMPLOYER');
+            }
+            else{
+                $user->setRole('ROLE_EMPLOYEE');
+            }
+            //$user->setRole($faker->randomElement($array = array('ROLE_EMPLOYER', 'ROLE_EMPLOYEE')));
             $user->setPhoneNum($faker->phoneNumber);
             $user->setCredits(200);
 
@@ -40,23 +48,24 @@ class UserFixture extends Fixture
 
             if(in_array('ROLE_EMPLOYEE', $user->getRoles()) == true)
             {
-                for($i = 0; $i < RANDOM_INT(1, 15); $i++)
+                for($j = 0; $j < RANDOM_INT(1, 3); $j++)
                 {
                     $resume = new Resume();
                     $resume->setUser($user);
-                    $resume->setArea($faker->text);
+                    $resume->setArea($faker->randomElement($array = array('Programuotojas', 'Apsauginis', 'Analitikas', 'Vairuotojas', 'Virejas')));
                     $resume->setAboutYou($faker->sentence);
                     $resume->setCreatedAt($faker->dateTime);
-                    $resume->setEducation($faker->country);
-                    $resume->setLanguages($faker->randomElements($array = array('Lithuanian', 'Russian', 'English', 'Polish'), $count = random_int(1,3)));
+                    $resume->setEducation($faker->randomElement($array = array('Pagrindinis', 'Vidurinis', 'Profesinis', 'Aukstasis')));
+                    $resume->setLanguages($faker->randomElements($array = array('Lietuviskai', 'Angliskai', 'Rusiskai', 'Vokiskai'), $count = random_int(1,3)));
                     $resume->setSalary(random_int(604, 3259));
                     $resume->setUpdatedAt($resume->getCreatedAt());
-                    $resume->setExperience(random_int(0, 5));
-                    $resume->setIsMain(true);
+                    $resume->setExperience($faker->randomElement($array = array('1', '2', '3', '4')));
+                    $resume->setIsMain($faker->boolean);
 
 
                     $manager->persist($resume);
                     $manager->flush();
+                    $user->setMainNum($resume->getId());
                 }
             }
 
